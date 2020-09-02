cb5e2075393e25e1e525d478a20d4b9a730318e4
denis20082000 <70638418+denis20082000@users.noreply.github.com>
2020-09-02 14:00:59 +0300

    Add files via upload

---
+++ MyProgram.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MyProgram.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world!!!" << endl; // prints
+	return 0;
+}
